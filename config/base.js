5661723e624dcd0d4d3aed8d79869b1b5faf8d63
Pavel - Ps <xxxx63644522.ben10@mail.ru>
2024-08-01 00:40:58 +0800

    Изменена структура lib, начально переработан UseDate.jsэ

--- config/base.js
+++ config/base.js
@@ -1,2 +1,3 @@
 export const VERSION = '2.2.0'
 export const MOBILES = /android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mmp|mobile|palm|phone|pie|tablet|up.browser|up.link|webos|wos/;
+export const LOCALIZATION = 'ru'
