1805e8ce160ab427c9e593dad3c1d9c86d06e8b7
Pavel - Ps <xxxx63644522.ben10@mail.ru>
2024-08-08 13:01:56 +0800

    Добавлен helper UseTools.js

--- src/shared/lib/main/useBase/useTools.js
+++ src/shared/lib/main/useBase/useTools.js
@@ -1,12 +1,11 @@
 /**
  * Проверяет, находится ли число (value) в указанном диапазоне.
- * @param {Object} params - Параметры функции.
- * @param {number} params.value - Значение, которое нужно проверить.
- * @param {number} params.min - Минимальное значение диапазона.
- * @param {number} params.max - Максимальное значение диапазона.
+ * @param {number} value - Значение, которое нужно проверить.
+ * @param {number} min - Минимальное значение диапазона.
+ * @param {number} max - Максимальное значение диапазона.
  * @returns {boolean} Возвращает true, если число находится в диапазоне, иначе false.
  */
-export const isInRange = ({ value, min, max }) => {
+export const isInRange = (value, min, max ) => {
     // Проверяем, что все параметры определены
     if (value == null || min == null || max == null) {
         return false
@@ -23,13 +22,12 @@ export const isInRange = ({ value, min, max }) => {
 
 /**
  * Генерирует случайное целое число в указанном диапазоне.
- * @param {Object} params - Параметры функции.
- * @param {number} [params.min=0] - Минимальное значение диапазона.
- * @param {number} [params.max=0] - Максимальное значение диапазона.
+ * @param {number} [min=0] - Минимальное значение диапазона.
+ * @param {number} [max=0] - Максимальное значение диапазона.
  * @returns {number} Случайное целое число в заданном диапазоне.
  */
-export const rand = ({ min = 0, max = 0 }) => {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+export const rand = (min = 0, max = 0) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
 
