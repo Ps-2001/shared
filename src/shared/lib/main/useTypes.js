12c177bfdc661a4f1b28a075388fd0e439655f8f
Pavel - Ps <xxxx63644522.ben10@mail.ru>
2024-08-08 12:57:13 +0800

    в UseTypes добавлен метод для определения типа - getType

--- src/shared/lib/main/useTypes.js
+++ src/shared/lib/main/useTypes.js
@@ -16,5 +16,21 @@ export const Type = {
   isNumber: value => !Number.isNaN(value) && typeof value === 'number',
   isBlob: value => Type.isObjectLike(value) && Type.isNumber(value.size) && Type.isString(value.type) && Type.isFunction(value.slice),
   isFile: value => Type.isBlob(value) && Type.isString(value.name) && (Type.isNumber(value.lastModified) || Type.isObjectLike(value.lastModifiedDate)),
-  getTag: value => Object.prototype.toString.call(value)
+  getTag: value => Object.prototype.toString.call(value),
+
+  getType: value => {
+    if (Type.isUndefined(value)) return 'undefined'
+    if (Type.isDomElem(value)) return 'HTMLElement'
+    if (Type.isNull(value)) return 'null'
+    if (Type.isArray(value)) return 'array'
+    if (Type.isObject(value)) return 'object'
+    if (Type.isFunction(value)) return 'function'
+    if (Type.isString(value)) return 'string'
+    if (Type.isNumber(value)) return 'number'
+    if (Type.isDate(value)) return 'date'
+    if (Type.isBlob(value)) return 'blob'
+    if (Type.isFile(value)) return 'file'
+    return undefined
+  }
+
 }
