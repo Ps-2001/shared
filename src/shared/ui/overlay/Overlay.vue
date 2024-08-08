516bd4ed77d810475c83003713d78dec3b134040
Pavel - Ps <xxxx63644522.ben10@mail.ru>
2024-08-08 13:11:55 +0800

    Добавлен компонент Overlay

---
+++ src/shared/ui/overlay/Overlay.vue
@@ -0,0 +1,26 @@
+<template>
+  <teleport to="body">
+    <div class="overlay"  @click="isClose && emit('close')">
+      <slot>
+
+      </slot>
+    </div>
+  </teleport>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+
+const props = defineProps({
+  isClose: {
+    type: Boolean,
+    default: true
+  }
+})
+const emit = defineEmits(['close'])
+
+</script>
+
+<style scoped lang="sass">
+@import "style"
+</style>
