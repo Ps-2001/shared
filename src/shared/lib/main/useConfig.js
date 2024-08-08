ae5e44d54b189b24a8a6f1f2e89768330fee0266
Pavel - Ps <xxxx63644522.ben10@mail.ru>
2024-08-08 23:21:37 +0800

    Добавлен helper по работе с конфигурацией проекта

--- src/shared/lib/main/useConfig.js
+++ src/shared/lib/main/useConfig.js
@@ -1,7 +1,7 @@
 export const getEnvVariable = (key) => {
 
     if (import.meta.env[key] === undefined) {
-        throw new Error(`Переменная окружения не найдена`);
+        throw new Error(`Переменная окружения не найдена`)
     }
 
     return import.meta.env[key]
