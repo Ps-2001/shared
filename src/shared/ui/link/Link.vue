9578e18af0a0e9669dd19bd0592808cbf9198035
Pavel - Ps <xxxx63644522.ben10@mail.ru>
2024-08-08 13:10:04 +0800

    В компонент Link добавлен пропс to

--- src/shared/ui/link/Link.vue
+++ src/shared/ui/link/Link.vue
@@ -1,22 +1,30 @@
 <template>
-  <div @click="handleClick" @mouseenter="handleMouseEnter" @mouseleave="handleMouseLeave">
+
+  <router-link v-if="to" :to="to">
+    <slot></slot>
+  </router-link>
+
+  <div v-else-if="href" @click="handleClick" @mouseenter="handleMouseEnter" @mouseleave="handleMouseLeave">
     <slot></slot>
   </div>
+
 </template>
 
 <script setup>
 import { defineProps } from 'vue'
-import { redirect } from "@/shared/lib/useURL.js"
+import {  } from 'vue-router'
+import { redirect } from "@/shared/lib/main/useURL.js"
 
 const props = defineProps({
   href: String,
+  to: String,
   status: String,
   callback: Function,
-});
+})
 
 const handleClick = (event) => {
   if (!props.href) event.preventDefault()
-  else redirect({ url: props.href })
+  else redirect(props.href)
 }
 
 const handleMouseEnter = () => {
