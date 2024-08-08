e7f35f3d8aa234de80e133a8ecb64c0d07f64e8e
Pavel - Ps <xxxx63644522.ben10@mail.ru>
2024-08-08 12:54:31 +0800

    Добавлен helper для работы со стилями. Добавлены методы для парсинга внешних и внутренних отступов

---
+++ src/shared/lib/main/useStyle.js
@@ -0,0 +1,55 @@
+import { Type } from "@/shared/lib/main/useTypes.js"
+import { layoutSpacingRegex } from "@/shared/config/regex/styles.js"
+
+/**
+ * Получить строку CSS для padding
+ * @param {Array|String} padding - Массив значений внутреннего отступа
+ * @returns {string} - Строка, представляющая значения внутреннего отступа в правильном формате
+ */
+export const getPadding = (padding) => {
+
+    let res = ''
+
+    if (!Type.isArray(padding) && !Type.isString(padding)) return ''
+
+    if (Type.isString(padding) && !layoutSpacingRegex.test(padding)) return ''
+
+    if (Type.isString(padding) && layoutSpacingRegex.test(padding)) padding = padding.split(' ')
+
+     switch (padding.length) {
+         case 1: res = `${padding[0]}`; break
+         case 2: res = `${padding[0]} ${padding[1]}`; break
+         case 3: res = `${padding[0]} ${padding[1]} ${padding[2]}`; break
+         case 4: res = `${padding[0]} ${padding[1]} ${padding[2]} ${padding[3]}`; break
+     }
+
+    return res
+
+}
+
+
+/**
+ * Получить строку CSS для margin
+ * @param {Array|String} margin - Массив значений внешнего отступа
+ * @returns {string} - Строка, представляющая значения внешнего отступа в правильном формате
+ */
+export const getMargin = (margin) => {
+
+    let res = ''
+
+    if (!Type.isArray(margin) && !Type.isString(margin)) return ''
+
+    if (Type.isString(margin) && !layoutSpacingRegex.test(margin)) return ''
+
+    if (Type.isString(margin) && layoutSpacingRegex.test(margin)) margin = margin.split(' ')
+
+    switch (margin.length) {
+        case 1: res = `${margin[0]}`; break
+        case 2: res = `${margin[0]} ${margin[1]}`; break
+        case 3: res = `${margin[0]} ${margin[1]} ${margin[2]}`; break
+        case 4: res = `${margin[0]} ${margin[1]} ${margin[2]} ${margin[3]}`; break
+    }
+
+    return res
+
+}
