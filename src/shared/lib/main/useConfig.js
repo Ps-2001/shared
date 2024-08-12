b168d80ffcc145ceea4187f2ebbd08fe30ff8514
Pavel - Ps <xxxx63644522.ben10@mail.ru>
2024-08-12 23:41:12 +0800

    В useConfig изменен метод: getEnvVariable. Добавленно описание метода, добален префикс VITE_ для названия переменной окружения

--- src/shared/lib/main/useConfig.js
+++ src/shared/lib/main/useConfig.js
@@ -1,8 +1,16 @@
+/**
+ * Получить переменную окружения по названию
+ * @param {string} key - Название переменной окружения без префикса VITE_
+ * @returns {string} - Значение переменной окружения
+ * @throws {Error} - Если переменная окружения не найдена
+ */
 export const getEnvVariable = (key) => {
 
-    if (import.meta.env[key] === undefined) {
-        throw new Error(`Переменная окружения не найдена`)
+    const name = `VITE_${key}`
+
+    if (import.meta.env[name] === undefined) {
+        throw new Error(`Переменная окружения ${name} не найдена`)
     }
 
-    return import.meta.env[key]
+    return import.meta.env[name]
 }
