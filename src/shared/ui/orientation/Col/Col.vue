7c359e0e2c1ec76183ea1fac0e366319dd8f96a0
Pavel - Ps <xxxx63644522.ben10@mail.ru>
2024-09-01 20:49:48 +0800

    Добавлены пропсы отступы: внутрение и наружные.

--- src/shared/ui/orientation/Col/Col.vue
+++ src/shared/ui/orientation/Col/Col.vue
@@ -1,22 +1,37 @@
 <template>
   <Flex
-    direction="row"
-    v-bind="props"
+      direction="row"
+      v-bind="props"
+      :style="style"
   >
     <slot></slot>
   </Flex>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { computed, ref, toRefs } from 'vue'
 import Flex from '../Flex/Flex.vue'
+import { getMargin, getPadding } from "@/shared/lib/main/useStyle.js"
 
 const props = defineProps({
   max: Boolean,
   wrap: String,
   gap: Number,
+  padding: [Array, String],
+  margin: [Array, String],
 })
 
+const { max, wrap, gap, disabled, padding, margin } = toRefs(props)
+
+const style = computed(() => {
+  const marginValue = getMargin(margin.value)
+  const paddingValue = getPadding(padding.value)
+
+  return {
+    ...({ margin: marginValue || {} }),
+    ...({ padding: paddingValue || {} }),
+  }
+})
 </script>
 
 <style scoped lang="sass">
--- src/shared/ui/orientation/Row/Row.vue
+++ src/shared/ui/orientation/Row/Row.vue
@@ -1,20 +1,36 @@
 <template>
   <Flex
-    direction="col"
-    v-bind="props"
+      direction="col"
+      v-bind="props"
+      :style="style"
   >
     <slot></slot>
   </Flex>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { computed, ref, toRefs } from 'vue'
 import Flex from '../Flex/Flex.vue'
+import { getMargin, getPadding } from "@/shared/lib/main/useStyle.js"
 
 const props = defineProps({
   max: Boolean,
   wrap: String,
   gap: Number,
+  padding: [Array, String],
+  margin: [Array, String],
+})
+
+const { max, wrap, gap, disabled, padding, margin } = toRefs(props)
+
+const style = computed(() => {
+  const marginValue = getMargin(margin.value)
+  const paddingValue = getPadding(padding.value)
+
+  return {
+    ...({ margin: marginValue || {} }),
+    ...({ padding: paddingValue || {} }),
+  }
 })
 
 </script>
