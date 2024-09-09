1d17ce35f48769bd1f08909926a28afc6644beed
Pavel - Ps <xxxx63644522.ben10@mail.ru>
2024-09-09 21:24:31 +0800

    Добавлен метод range

--- src/shared/lib/main/useBase/useTools.js
+++ src/shared/lib/main/useBase/useTools.js
@@ -5,6 +5,8 @@
  * @param {number} max - Максимальное значение диапазона.
  * @returns {boolean} Возвращает true, если число находится в диапазоне, иначе false.
  */
+import {Type} from "@/shared/lib/main/useTypes.js"
+
 export const isInRange = (value, min, max ) => {
     // Проверяем, что все параметры определены
     if (value == null || min == null || max == null) {
@@ -30,6 +32,39 @@ export const rand = (min = 0, max = 0) => {
     return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
+/**
+ * Получить диапазон чисел в виде массива.
+ *
+ * Если передан только параметр `start`, диапазон будет от 0 до `start`.
+ * Если `step` не является положительным числом, он по умолчанию равен 1.
+ *
+ * @param {number} start - Начало диапазона. Если `end` не определен, `start` считается `end`, и диапазон начинается с 0.
+ * @param {number} [end] - Конец диапазона.
+ * @param {number} [step=1] - Шаг между числами в диапазоне (положительное число).
+ * @returns {number[]} Массив, содержащий числа в указанном диапазоне.
+ */
+export const range = (start, end, step = 1) => {
+
+    if (start === undefined && end === undefined) return []
+
+    if (Type.isNumber(+start) && !Type.isNumber(+end)) {
+        end = start
+        start = 0
+    }
+
+    if (+start > +end) return []
+
+    if (!Type.isNumber(+step) || +step <= 0) step = 1
+
+    const result = []
+
+    for (let i = start; i < end; i += step) {
+        result.push(i)
+    }
+
+    return result
+
+}
 
 export const calculation = ({  }) => {
 
